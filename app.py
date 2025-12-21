6d21333485ae3ea80d0d7ea0760cbdcccb06c7cb
Rohit_More <morer4851@gmail.com>
2025-12-21 19:30:22 +0530

    Created database to store user information

--- app.py
+++ app.py
@@ -1,7 +1,25 @@
 from flask import Flask, render_template
 from flask import request
 
+from flask_sqlalchemy import SQLAlchemy
+
 app = Flask(__name__)
+
+# Configure the app to protect it from hackers, trackers, hijackers, etc.
+app.config["SECRET_KEY"] = "myApplication123"
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///data.db"
+db = SQLAlchemy(app)
+
+
+# Create database model
+class Form(db.Model):
+	id = db.Column(db.Integer, primary_key=True)
+	first_name = db.Column(db.String(80))
+	last_name = db.Column(db.String(80))
+	email = db.Column(db.String(80))
+	date = db.Column(db.Date)
+	occupation = db.Column(db.String(80))
+
 
 @app.route('/', methods=["GET", "POST"])
 def index():
@@ -16,4 +34,6 @@ def index():
 
 
 if __name__ == "__main__":
-	app.run(debug=True, port=5001)
+	with app.app_context():
+		db.create_all()
+		app.run(debug=True, port=5001)
