eb97f61869c644159a450b2d5ef9674ec51d957b
Rohit_More <morer4851@gmail.com>
2025-12-21 19:12:35 +0530

    Implemented flask to get filled form information

--- app.py
+++ app.py
@@ -1,11 +1,21 @@
 from flask import Flask, render_template
-
+from flask import request
 
 app = Flask(__name__)
 
-@app.route('/')
+@app.route('/', methods=["GET", "POST"])
 def index():
+	if request.method == "POST":
+		first_name = request.form['firstName']
+		last_name = request.form['lastName']
+		email = request.form['email']
+		start_date = request.form['startDate']
+		occupation = request.form['occupation']
+		
+	
 	return render_template('index.html')
+
+
 
 
 if __name__ == "__main__":
