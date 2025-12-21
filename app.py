5cf4df0c14efff39bac23fa6028850af48cfff1c
Rohit_More <morer4851@gmail.com>
2025-12-21 20:21:53 +0530

    Sent the successful submission notice and submitted information to user via gmail

--- app.py
+++ app.py
@@ -1,21 +1,34 @@
 from flask import Flask, render_template
 from flask import request
 from flask import flash
-
 from flask_sqlalchemy import SQLAlchemy
+from flask_mail import Mail, Message
 
 from datetime import datetime
+
+from info import password, sender
 
 app = Flask(__name__)
 
 # Configure the app to protect it from hackers, trackers, hijackers, etc.
 app.config["SECRET_KEY"] = "myApplication123"
 app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///data.db"
+app.config["MAIL_SERVER"] = "smtp.gmail.com"
+app.config["MAIL_PORT"] = 465           # port number for gmail
+app.config["MAIL_USE_SSL"] = True
+app.config["MAIL_USERNAME"] = sender
+app.config["MAIL_PASSWORD"] = password
+
+# Create the instance of database
 db = SQLAlchemy(app)
+
+# Create mail instance
+mail = Mail(app)
 
 
 # Create database model
 class Form(db.Model):
+	# Create table for database
 	id = db.Column(db.Integer, primary_key=True)
 	first_name = db.Column(db.String(80))
 	last_name = db.Column(db.String(80))
@@ -26,6 +39,7 @@ class Form(db.Model):
 
 @app.route('/', methods=["GET", "POST"])
 def index():
+	# Get user information if method is 'POST'
 	if request.method == "POST":
 		# Get the user information
 		first_name = request.form['firstName']
@@ -41,6 +55,19 @@ def index():
 		db.session.add(form)
 		db.session.commit()
 		
+		# Send the mail
+		message_body = f"Thank you for your submission, {first_name}." \
+		               f"Here is your data:\n{first_name}\n{last_name}\n{start_date}\n" \
+		               f"Thank you!"
+		
+		message = Message(subject="New form submission",
+		                  sender=app.config["MAIL_USERNAME"],
+		                  recipients=[email],
+		                  body=message_body)
+		
+		mail.send(message)
+		
+		# Display successful submission of form and email sent.
 		flash(f"{first_name} , your form was submitted successfully.", "success")
 		
 	return render_template('index.html')
